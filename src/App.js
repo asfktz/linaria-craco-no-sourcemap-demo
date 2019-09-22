6d36bde3686d1e64b73da05ec0740db80733ab94
asfktz <asf.ktz@gmail.com>
2019-09-22 04:31:32 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { styled } from 'linaria/react';
+
+const Test = styled.div`
+  font-size: 3em;
+  color: purple;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test>
+        Test
+      </Test>
     </div>
   );
 }
